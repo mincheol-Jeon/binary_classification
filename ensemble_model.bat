29d4c5751275499293525427971d467e9613787d
mincheol-Jeon <alsehf0714@naver.com>
2023-06-12 19:02:36 +0900

    make batch file(not worked)

---
+++ ensemble_model.bat
@@ -0,0 +1,4 @@
+@echo off
+call C:\ProgramData\Anaconda3\condabin\conda activate base
+call python C:\Users\alseh\Desktop\jeonmincheol\2023\2023-1\binary_classification\emsemble_model_for_batch.py %*
+pause
